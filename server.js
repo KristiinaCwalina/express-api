375313cf7af642701ce44db3511d675827669b77
housni <alami.housni@gmail.com>
2020-02-15 14:09:11 +0100

    GET, PUT, DELETE, POST

--- server.js
+++ server.js
@@ -34,9 +34,20 @@ const albumsData = [
     artworkUrl100:
       "http://is1.mzstatic.com/image/thumb/Music/v4/18/93/6d/18936d85-8f6b-7597-87ef-62c4c5211298/source/100x100bb.jpg",
     releaseDate: "2003-06-24T07:00:00Z",
-    primaryGenreName: "Pop",
+    primaryGenreName: "Rock",
     url:
       "https://www.youtube.com/embed/ViwtNLUqkMY?rel=0&amp;controls=0&amp;showinfo=0"
+  },
+  {
+    albumId: "13",
+    artistName: "Beyoncé",
+    collectionName: "Lemonade3",
+    artworkUrl100:
+      "http://is1.mzstatic.com/image/thumb/Music20/v4/23/c1/9e/23c19e53-783f-ae47-7212-03cc9998bd84/source/100x100bb.jpg",
+    releaseDate: "2016-04-25T07:00:00Z",
+    primaryGenreName: "RnB",
+    url:
+      "https://www.youtube.com/embed/PeonBmeFR8o?rel=0&amp;controls=0&amp;showinfo=0"
   }
 ];
 
@@ -54,7 +65,17 @@ app.get("/albums/:albumId", function(req, res) {
 });
 
 app.get("/albums", function(req, res) {
-  res.send(albumsData);
+  if (req.query.genre) {
+    const albums = albumsData.filter(function(album) {
+      return (
+        album.primaryGenreName.toLowerCase() === req.query.genre.toLowerCase()
+      );
+    });
+    //console.log(req.query.genre);
+    res.send(albums);
+  } else {
+    res.send(albumsData);
+  }
 });
 
 app.post("/albums", function(req, res) {
@@ -64,9 +85,22 @@ app.post("/albums", function(req, res) {
 });
 
 app.delete("/albums/:albumId", function(req, res) {
-  const deleteAlbum = album[req.params.id];
-  delete album[req.params.id];
-  res.send(JSON.stringify(deleteAlbum));
+  //const albumID = req.params.albumId;
+  const index = albumsData.findIndex(function(album) {
+    return album.albumId === req.params.albumId;
+  });
+  albumsData.splice(index, 1);
+
+  res.send(201);
+});
+
+app.put("/albums/:albumId", function(req, res) {
+  //const id = req.params.albumId;
+  const index = albumsData.findIndex(function(album) {
+    return album.albumId === req.params.albumId;
+  });
+  albumsData[index] = req.body;
+  res.send(200);
 });
 
 app.listen(3000, function() {
